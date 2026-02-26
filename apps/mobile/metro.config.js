5a75e43df15385044f79453b5814386d137080e4
makuntz <mkprovida@gmail.com>
2026-02-26 07:49:34 -0300

    fix: initials configs to expo go

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,8 +1,16 @@
 // Learn more https://docs.expo.dev/guides/customizing-metro
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(projectRoot);
+
+// Configuração para monorepo
+config.watchFolders = [workspaceRoot];
+config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
 
